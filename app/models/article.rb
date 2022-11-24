c0e89567b206ffbfd164ca7c44432925cfc89160
chirag <chirag.g@simplyalliancess.com>
2022-11-24 21:34:21 +0530

    create aricle

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,5 @@
 class Article < ApplicationRecord
   searchkick word_start: [:title], highlight: [:title] unless Rails.env.eql?('test')
+   validates :title, presence: true
+  validates :body, presence: true, length: { minimum: 10 }
 end
